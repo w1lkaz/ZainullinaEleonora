391edeeac743c7835dd52ce07c9a788548428f75
no7 <thefosyachannel@gmail.com>
2025-10-26 16:52:21 +0500

    добавила строчку из любимой песни

--- zainullinaivtpo202b.py
+++ zainullinaivtpo202b.py
@@ -2,5 +2,6 @@ def main():
     print('Зайнуллина Элеонора')
     print('Моя группа: ИВТ-ПО-202Б')
     print('Дата выполнения: 26.10.2025')
+    print('Cut through This razor wire And dine on your heart Mine til the end')
 if __name__ == '__main__':
     main()
